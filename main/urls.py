06a0097874d2ceadf8e2bc61750af8b0a3fc725e
rupeish <rupeshspatil26@gmail.com>
2020-01-25 14:21:32 +0530

    fixed url error

--- main/urls.py
+++ main/urls.py
@@ -20,7 +20,7 @@ app_name="main"
 
 urlpatterns = [
 	path('', views.homepage, name='homepage'),
-	path('register/', views.register, name='register'),
+	path('register', views.register, name='register'),
 	path('logout', views.logout_request, name='logout'),
 	path('login', views.login_request, name='login'),
 	path('<single_slug>', views.single_slug, name='single_slug')
